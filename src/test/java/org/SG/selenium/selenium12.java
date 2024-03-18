9c306a22c418403280e35ffb76616f442da29dc1
AnshuSG <gantianshu@gmail.com>
2024-03-18 08:01:55 +0530

    2 negative automation testcases

---
+++ src/test/java/org/SG/selenium/selenium12.java
@@ -0,0 +1,66 @@
+package org.SG.selenium;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class selenium12 {
+
+    @Test(groups = "QA")
+    @Description("Negative TC#1 , Invalid Username and valid password")
+    public void TCNegative1(){
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+        WebElement emailinput = driver.findElement(By.id("login-username"));
+        emailinput.sendKeys("admin@admin.com");
+
+        WebElement passwordinput = driver.findElement(By.id("login-password"));
+        passwordinput.sendKeys("admin");
+
+        driver.findElement(By.id("js-login-btn")).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement error_msg  =  driver.findElement(By.className("notification-box-description"));
+        String errormsg_text = error_msg.getText();
+
+        Assert.assertEquals(errormsg_text,"Your email, password, IP address or location did not match");
+        driver.quit();
+    }
+
+    @Test(groups = "QA")
+    @Description("Negative TC#2 , Valid Username and invalid password")
+    public void TCNegative2(){
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+        WebElement emailinput = driver.findElement(By.id("login-username"));
+        emailinput.sendKeys("dharanshu6@gmail.com");
+
+        WebElement passwordinput = driver.findElement(By.id("login-password"));
+        passwordinput.sendKeys("admin");
+
+        driver.findElement(By.id("js-login-btn")).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement error_msg  =  driver.findElement(By.className("notification-box-description"));
+        String errormsg_text = error_msg.getText();
+
+        Assert.assertEquals(errormsg_text,"Your email, password, IP address or location did not match");
+        driver.quit();
+    }
+
+}
